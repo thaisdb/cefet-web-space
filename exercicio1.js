057fb7dee9b94b49e6ea985f0c023a6f64186347
thaisdb <thais.dinizbraz@gmail.com>
2017-04-11 12:23:37 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,23 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let buttons = document.getElementsByClassName('botao-expandir-retrair');
+
+for (let button of buttons){
+	button.addEventListener('click', buttonAction);
+}
+
+function buttonAction() {
+
+	let paragrafo = this.parentNode;
+	if(paragrafo.classList.contains('expandido')){
+		paragrafo.classList.remove('expandido');
+		this.innerHTML = '+';
+
+	} else {
+		paragrafo.classList.add('expandido');
+		this.innerHTML = '-';
+
+	}
+
+}
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,12 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let ant = document.getElementById('anterior')
+let prox = document.getElementById('proximo')
+
+ant.addEventListener('click', before)
+
+function before(){
+	document.getElementById('slide').src = servidorDasImagens.concat(todasasImagens--)
+}
